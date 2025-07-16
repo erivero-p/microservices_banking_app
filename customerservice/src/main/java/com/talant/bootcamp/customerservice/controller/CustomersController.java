014919e475912b93837a9448647435cfd211bd18
Isaac Pozo Urgilés <isaac.pozo@talan.com>
2025-07-16 11:58:33 +0200

    controlador allCostumers

--- customerservice/src/main/java/com/talant/bootcamp/customerservice/controller/CustomersController.java
+++ customerservice/src/main/java/com/talant/bootcamp/customerservice/controller/CustomersController.java
@@ -1,4 +1,24 @@
 package com.talant.bootcamp.customerservice.controller;
 
+import com.talant.bootcamp.customerservice.models.dto.CustomerDTO;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@RequestMapping("/api/customers")
+@RestController
 public class CustomersController {
+
+
+    @GetMapping
+    @ResponseStatus(HttpStatus.OK)
+    public List<CustomerDTO> getAllCustomers(){
+        return new ArrayList<>();
+    }
+
 }
