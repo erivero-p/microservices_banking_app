5b637815ab7701bf731998790def8cc69950e1a7
Yulia Bila <yuliia.bila@talan.com>
2025-07-25 10:56:23 +0200

    test: account servise unit test: create account

--- accountservice/src/test/java/com/talant/bootcamp/accountservice/service/AccountServiceTest.java
+++ accountservice/src/test/java/com/talant/bootcamp/accountservice/service/AccountServiceTest.java
@@ -3,6 +3,7 @@ package com.talant.bootcamp.accountservice.service;
 import com.talant.bootcamp.accountservice.dto.AccountDTO;
 import com.talant.bootcamp.accountservice.entity.Account;
 import com.talant.bootcamp.accountservice.entity.AccountState;
+import com.talant.bootcamp.accountservice.mapper.AccountMapper;
 import com.talant.bootcamp.accountservice.repository.AccountRepository;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
@@ -23,6 +24,7 @@ import java.util.UUID;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 @ExtendWith(MockitoExtension.class)
@@ -30,6 +32,8 @@ class AccountServiceTest {
 
     @Mock
     private AccountRepository accountRepository;
+    @Mock
+    private AccountMapper accountMapper;
 
     @InjectMocks
     private AccountService accountService;
@@ -41,7 +45,7 @@ class AccountServiceTest {
     void setAccount(){
       request = new AccountDTO(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now(), AccountState.ACTIVE, 0L);
 
-      account = new Account(UUID.randomUUID(), UUID.randomUUID(), LocalDate.now(), AccountState.ACTIVE, 0L);
+      account = new Account(request.id(), request.customerId(), LocalDate.now(), AccountState.ACTIVE, 0L);
     }
 
     @Test
@@ -51,22 +55,37 @@ class AccountServiceTest {
         Page<Account> accountPage = new PageImpl<>(List.of(account));
 
         when(accountRepository.findAll(pageable)).thenReturn(accountPage);
-        when(accountRepository.save(any(Account.class))).thenReturn(account);
+        when(accountMapper.toAccountDTO(account)).thenReturn(request);
 
 
         Page <AccountDTO> result = accountService.findAll(pageable);
 
-        //assertEquals(1, result.getSize());
+        assertEquals(1, result.getSize());
         assertEquals(account.getId(), result.getContent().getFirst().id());
 
     }
 
     @Test
-    void findById() {
+    void shouldCreateNewAccount() {
+
+        when(accountMapper.toAccountEntity(request)).thenReturn(account);
+        when(accountRepository.save(account)).thenReturn(account);
+        when(accountMapper.toAccountDTO(account)).thenReturn(request);
+
+        AccountDTO response = accountService.create(request);
+
+        assertNotNull(response);
+        assertEquals(account.getId(), response.id());
+        assertEquals(account.getAccountBalance(), response.accountBalance());
+        assertEquals(account.getAccountState(), response.accountState());
+        assertEquals(account.getCreationDate(), response.creationDate());
+        assertEquals(account.getCustomerId(), response.customerId());
+
+        verify(accountRepository).save(any(Account.class));
     }
 
     @Test
-    void create() {
+    void findById() {
     }
 
     @Test
